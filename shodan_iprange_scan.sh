fc05200b87e2a529fa706d28735eea71c27cdaca
Prometheus918 <50356923+Prometheus918@users.noreply.github.com>
2021-04-02 01:47:01 +0530

    Update shodan_iprange_scan.sh

--- shodan_iprange_scan.sh
+++ shodan_iprange_scan.sh
@@ -4,7 +4,5 @@ mkdir $(date -I)_shodan
 cd $(date -I)_shodan
 while IFS= read -r line; do
     echo "Text read from file: $line"
-    mkdir $line 
-    cd $line
-    shodan scan submit --filename scan-results $line
+    shodan scan submit --filename $line $line
 done < "$1"
